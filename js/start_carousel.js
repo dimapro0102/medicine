1fefd4b5d5b956fe61c4fcb97d5c5a38bfc6c096
dimapro0102 <139034229+dimapro0102@users.noreply.github.com>
2023-07-09 08:59:40 +0300

    Add files via upload

---
+++ js/start_carousel.js
@@ -0,0 +1,15 @@
+        $(document).ready(function(){
+			$(".slider_promotion").owlCarousel({
+				items: 1,
+				loop: true,
+				nav: true,
+				autoplay: true,
+				autoplayTimeout: 7000,
+				autoplayHoverPause: true,
+				navText: ["",""],
+				autoplaySpeed: 1000,
+				navSpeed: 1000,
+				dots: true,
+				dotsSpeed: 1000
+			});
+		});
